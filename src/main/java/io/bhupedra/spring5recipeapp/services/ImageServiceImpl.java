a3a033ccb31e2ebf1599b3b98342151c1987f1d0
bhupendra <bhupendrapt95@gmail.com>
2020-04-25 13:11:29 +0530

    Saving Image to DB file.

--- src/main/java/io/bhupedra/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/io/bhupedra/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,10 +1,13 @@
 package io.bhupedra.spring5recipeapp.services;
 
+import io.bhupedra.spring5recipeapp.domain.Recipe;
 import io.bhupedra.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @Slf4j
@@ -20,6 +23,26 @@ public class ImageServiceImpl implements ImageService{
     @Transactional
     public void saveImage(Long recipeId, MultipartFile file) {
 
-        log.debug("Saving Image on Recipe id :" +recipeId );
+        log.debug("Saving Image on Recipe id :" +recipeId );        try {
+
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+
+            log.debug("Saving Image on Recipe id :" + recipeId);
+
+        } catch (IOException ex) {
+            //todo handle better
+            log.error("error occurred");
+            ex.printStackTrace();
+        }
+
     }
 }
