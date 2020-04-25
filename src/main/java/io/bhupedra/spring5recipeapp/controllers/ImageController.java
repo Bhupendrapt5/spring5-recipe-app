edf8cf9014e9373a8d0ff3c084523719c63e07a6
bhupendra <bhupendrapt95@gmail.com>
2020-04-25 15:27:13 +0530

    Rendering Recipe Image from Db

--- src/main/java/io/bhupedra/spring5recipeapp/controllers/ImageController.java
+++ src/main/java/io/bhupedra/spring5recipeapp/controllers/ImageController.java
@@ -1,8 +1,10 @@
 package io.bhupedra.spring5recipeapp.controllers;
 
+import io.bhupedra.spring5recipeapp.commands.RecipeCommand;
 import io.bhupedra.spring5recipeapp.services.ImageService;
 import io.bhupedra.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Slf4j
 @Controller
@@ -40,5 +47,27 @@ public class ImageController {
         imageService.saveImage(Long.valueOf(id),file);
 
         return "redirect:/recipe/"+id+"/show";
+    }
+
+    @GetMapping("/recipe/{recipeId}/recipeimage")
+    public void renderImageFormDb(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
+
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+
+        if(recipeCommand.getImage()!=null){
+            log.debug("rendering Image from Db");
+
+            byte[] bytes = new byte[recipeCommand.getImage().length];
+            int i = 0;
+
+            for (byte wrapByte : recipeCommand.getImage()){
+
+                bytes[i++] = wrapByte; //Auto Unboxing
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream stream = new ByteArrayInputStream(bytes);
+            IOUtils.copy(stream, response.getOutputStream());
+        }
     }
 }
--- src/test/java/io/bhupedra/spring5recipeapp/controllers/ImageControllerTest.java
+++ src/test/java/io/bhupedra/spring5recipeapp/controllers/ImageControllerTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -59,5 +61,35 @@ class ImageControllerTest {
                 .andExpect(header().string("Location", "/recipe/1/show"));
 
         verify(imageService, times(1)).saveImage(anyLong(), any());
+    }
+
+    @Test
+    void testRenderImageFromDb() throws Exception {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        String imgString = "Fake Image String";
+
+        Byte[] bytesArray = new Byte[imgString.getBytes().length];
+        int i = 0;
+
+        for(Byte aByte : imgString.getBytes()){
+            bytesArray[i++] = aByte;
+        }
+         recipeCommand.setImage(bytesArray);
+
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //when
+        MockHttpServletResponse mockResponse = mockMvc.perform(get("/recipe/1/recipeimage"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        //then
+        byte[] responseArray = mockResponse.getContentAsByteArray();
+
+        assertEquals(imgString.getBytes().length, responseArray.length);
+
     }
 }
